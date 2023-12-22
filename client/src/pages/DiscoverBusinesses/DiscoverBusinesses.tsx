9469bb838c63bce47a5193238710430ae1895ea3
Oscar Waka <oscar.waka@orkestrascs.com>
2023-12-22 03:24:45 -0800

    resolved build issue

--- client/src/pages/DiscoverBusinesses/DiscoverBusinesses.tsx
+++ client/src/pages/DiscoverBusinesses/DiscoverBusinesses.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import { getListOfBusinsessProfile } from "api/business";
 import BusinessCatalogue from "./BusinessCatalogue/BusinessCatalogue";
