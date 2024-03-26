ba9c0d301a65d6d01821635fe2f19295ff8d8dde
benrobo <alumonabenaiah71@gmail.com>
2024-03-26 19:39:02 +0100

    recreating a new filter component

---
+++ client/src/components/BusinessFilter/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.scss";
+
+export default function BusinessesFilterComponent() {
+  return (
+    <div className="filter-container">
+      <div className="header"></div>
+    </div>
+  );
+}
---
+++ client/src/pages/ExploreBusiness/components/SearchComp.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchBusinessComp() {
+  return <div>SearchBusinessComp</div>;
+}
