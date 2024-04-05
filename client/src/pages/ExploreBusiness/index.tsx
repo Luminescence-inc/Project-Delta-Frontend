fb0427a66e48b6e47cabcddcecb4ede6df629bd7
benrobo <alumonabenaiah71@gmail.com>
2024-04-05 13:31:27 +0100

    Refactor ExploreBusiness component

--- client/src/pages/ExploreBusiness/index.tsx
+++ client/src/pages/ExploreBusiness/index.tsx
@@ -5,7 +5,7 @@ import {
   FlexRowCenterBtw,
   FlexRowStartCenter,
 } from "components/Flex";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./style.scss";
 import { LoaderCircle } from "lucide-react";
 import SearchIcon from "assets/icons/search-business.svg?react";
@@ -13,44 +13,24 @@ import LayoutPanelTop from "assets/icons/layout-panel-top.svg?react";
 import LayoutPanelLeft from "assets/icons/layout-panel-left.svg?react";
 import BusinessCardContainer from "./components/BusinessCard";
 import BusinessesFilterComponent from "components/BusinessFilter";
-import { allBusinessCategories, getListOfBusinsessProfile } from "api/business";
-import {
-  BusinessCategories,
-  BusinessListingLayouts,
-  IOption,
-  UserBusinessList,
-} from "types/business";
-import { FilterData } from "context/BusinessCtx";
-import { IBusinessProfile, IFilter, ISearch } from "types/business-profile";
+import { BusinessListingLayouts, UserBusinessList } from "types/business";
+import { FilterData, useBusinessCtx } from "context/BusinessCtx";
+import { IFilter } from "types/business-profile";
 import { cn } from "utils";
 
 const ExploreBusiness = () => {
+  const {
+    businessCategory,
+    businesses,
+    getBusinesses,
+    currPage,
+    allBusinessesLoading,
+    totalPages,
+    setSearchQuery,
+    showFilter,
+    setShowFilter,
+  } = useBusinessCtx();
   const [layout, setLayout] = useState<BusinessListingLayouts>("col");
-  const [showFilter, setShowFilter] = useState<boolean>(true);
-  const [businessCategory, setBusinessCategory] = useState<IOption[]>();
-  const [currPage, setCurrPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(1);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [searchQuery, setSearchQuery] = useState<ISearch | null>(null);
-  const [businesses, setBusinesses] = useState<IBusinessProfile[] | []>([]);
-
-  useEffect(() => {
-    try {
-      allBusinessCategories().then((res) => {
-        const resData: BusinessCategories = res.data;
-        setBusinessCategory(
-          resData.data.businessCategories.map((businessCat) => {
-            return { uuid: businessCat.uuid, value: businessCat.description };
-          })
-        );
-      });
-    } catch (err) {}
-  }, []);
-
-  // fetch all businesses initially with or without a filter
-  useEffect(() => {
-    getBusinesses(1, searchQuery ? true : false);
-  }, [searchQuery]);
 
   // construct the search query
   const constructQuery = (filterData: FilterData) => {
@@ -73,36 +53,6 @@ const ExploreBusiness = () => {
     });
   };
 
-  const getBusinesses = async (currPage: number, filterApplied: boolean) => {
-    setLoading(true);
-    const result = await getListOfBusinsessProfile(
-      {
-        page: currPage,
-        sortBy: "createdUtc",
-        sortDirection: "asc",
-        limit: 10,
-      },
-      searchQuery
-    );
-    const data = result.data?.data.businessProfiles;
-
-    setLoading(false);
-
-    // remove any duplicates
-    if (!filterApplied) {
-      const comb = [...businesses, ...data?.data];
-      const unique = comb.filter(
-        (v, i, a) => a.findIndex((t) => t.uuid === v.uuid) === i
-      );
-      setBusinesses(unique);
-    } else {
-      setBusinesses(data.data);
-    }
-
-    setTotalPages(data?.totalPages || 1);
-    setCurrPage(currPage);
-  };
-
   return (
     <div className="ntw w-full h-full">
       <FlexColStart className="w-full px-20 mt-10">
@@ -121,10 +71,10 @@ const ExploreBusiness = () => {
         <button
           className={cn(
             "ntw w-full px-15 py-15 rounded-10 border-none font-helvetical filter-trigger",
-            loading ? "disabled" : "cursor-pointer bg-white"
+            allBusinessesLoading ? "disabled" : "cursor-pointer bg-white"
           )}
           onClick={() => setShowFilter(true)}
-          disabled={loading}
+          disabled={allBusinessesLoading}
         >
           <FlexRowStartCenter className="w-full">
             <SearchIcon />
@@ -153,12 +103,15 @@ const ExploreBusiness = () => {
           height: "20px",
         }}
       >
-        {loading && <LoaderCircle size={15} className="loader" />}
+        {allBusinessesLoading && <LoaderCircle size={15} className="loader" />}
 
         {/* not found msg */}
-        {!loading && businesses.length === 0 && (
-          <FlexColStartCenter className="w-full">
-            <p className="ntw text-15 font-normal" style={{ color: "#9090A7" }}>
+        {!allBusinessesLoading && businesses.length === 0 && (
+          <FlexColStartCenter className="w-full mt-35">
+            <p
+              className="ntw text-15 font-bold font-hn-light"
+              style={{ color: "#9090A7" }}
+            >
               No business found
             </p>
           </FlexColStartCenter>
@@ -183,13 +136,13 @@ const ExploreBusiness = () => {
           <button
             className={cn(
               "ntw px-20 py-10 rounded-5 border-none outline-none flex items-center justify-center gap-1 loadmore-btn",
-              loading ? "disabled" : ""
+              allBusinessesLoading ? "disabled" : ""
             )}
             onClick={() => {
               getBusinesses(currPage + 1, false);
             }}
           >
-            {loading ? (
+            {allBusinessesLoading ? (
               <LoaderCircle size={15} className="loader" />
             ) : (
               <span className="ntw text-14 font-normal">Load more</span>
