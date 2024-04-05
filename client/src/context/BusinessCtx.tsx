7695d08b6ab77c9782a062ffd1a4b874db43a3c0
benrobo <alumonabenaiah71@gmail.com>
2024-04-05 13:31:20 +0100

    Refactor BusinessCtx component and add new functionality

--- client/src/context/BusinessCtx.tsx
+++ client/src/context/BusinessCtx.tsx
@@ -1,21 +1,23 @@
-import React, { PropsWithChildren, useState } from "react";
-import { ISearch } from "types/business-profile";
+import { allBusinessCategories, getListOfBusinsessProfile } from "api/business";
+import React, { PropsWithChildren, useEffect, useState } from "react";
+import { BusinessCategories, IOption } from "types/business";
+import { IBusinessProfile, ISearch } from "types/business-profile";
 
 export const BusinessContext = React.createContext<ContextValues>({} as any);
 
-type FilterOption = {
+interface FilterOption {
   uuid: string;
   value: string;
-};
+}
 
-export type FilterData = {
+export interface FilterData {
   businessCategoryUuid: { uuid?: string; value?: string }[] | undefined;
   stateAndProvince: { uuid: string } | undefined;
   city: { uuid: string } | undefined;
   country: { uuid: string } | undefined;
-};
+}
 
-type ContextValues = {
+interface ContextValues {
   searchQuery: ISearch | null;
   setSearchQuery: (searchQuery: ISearch | null) => void;
   activePanel: string;
@@ -26,14 +28,35 @@ type ContextValues = {
   setFilteredStates: (states: FilterOption[]) => void;
   filterData: FilterData;
   setFilterData: (filterData: FilterData) => void;
-};
+  businesses: IBusinessProfile[] | [];
+  setBusinesses: (businesses: IBusinessProfile[] | []) => void;
+  businessCategory: IOption[] | undefined;
+  setBusinessCategory: (businessCategory: IOption[] | undefined) => void;
+  allBusinessesLoading: boolean;
+  setAllBusinessesLoading: (loading: boolean) => void;
+  currPage: number;
+  setCurrPage: (page: number) => void;
+  totalPages: number;
+  setTotalPages: (pages: number) => void;
+  getBusinesses: (currPage: number, filterApplied: boolean) => void;
+  showFilter: boolean;
+  setShowFilter: (showFilter: boolean) => void;
+}
 
 interface BusinessContextProviderProps extends PropsWithChildren {}
 
 export default function BusinessContextProvider({
   children,
 }: BusinessContextProviderProps) {
+  const [businesses, setBusinesses] = useState<IBusinessProfile[] | []>([]);
+  const [allBusinessesLoading, setAllBusinessesLoading] =
+    useState<boolean>(false);
+  const [currPage, setCurrPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [businessCategory, setBusinessCategory] = useState<IOption[]>();
+
   // for filtering business profiles
+  const [showFilter, setShowFilter] = useState<boolean>(true);
   const [searchQuery, setSearchQuery] = useState<ISearch | null>(null);
   const [activePanel, setActivePanel] = React.useState<string>("");
   const [filteredCities, setFilteredCities] = React.useState<any>([]);
@@ -44,6 +67,55 @@ export default function BusinessContextProvider({
     city: undefined,
     country: undefined,
   });
+
+  // all business categories
+  useEffect(() => {
+    try {
+      allBusinessCategories().then((res) => {
+        const resData: BusinessCategories = res.data;
+        setBusinessCategory(
+          resData.data.businessCategories.map((businessCat) => {
+            return { uuid: businessCat.uuid, value: businessCat.description };
+          })
+        );
+      });
+    } catch (err) {}
+  }, []);
+
+  // fetch all businesses initially with or without a filter
+  useEffect(() => {
+    getBusinesses(1, searchQuery ? true : false);
+  }, [searchQuery]);
+
+  const getBusinesses = async (currPage: number, filterApplied: boolean) => {
+    setAllBusinessesLoading(true);
+    const result = await getListOfBusinsessProfile(
+      {
+        page: currPage,
+        sortBy: "createdUtc",
+        sortDirection: "asc",
+        limit: 10,
+      },
+      searchQuery
+    );
+    const data = result.data?.data.businessProfiles;
+
+    setAllBusinessesLoading(false);
+
+    // remove any duplicates
+    if (!filterApplied) {
+      const comb = [...businesses, ...data?.data];
+      const unique = comb.filter(
+        (v, i, a) => a.findIndex((t) => t.uuid === v.uuid) === i
+      );
+      setBusinesses(unique);
+    } else {
+      setBusinesses(data.data);
+    }
+
+    setTotalPages(data?.totalPages || 1);
+    setCurrPage(currPage);
+  };
 
   const ctxValues = {
     searchQuery,
@@ -56,6 +128,19 @@ export default function BusinessContextProvider({
     setFilteredStates,
     filterData,
     setFilterData,
+    businesses,
+    setBusinesses,
+    businessCategory,
+    setBusinessCategory,
+    allBusinessesLoading,
+    setAllBusinessesLoading,
+    currPage,
+    setCurrPage,
+    totalPages,
+    setTotalPages,
+    getBusinesses,
+    showFilter,
+    setShowFilter,
   } satisfies ContextValues;
 
   return (
