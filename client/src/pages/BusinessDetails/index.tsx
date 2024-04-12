f64b9d99947261ae1de89d8437e52b5dd2b843f6
benrobo <alumonabenaiah71@gmail.com>
2024-04-12 18:23:06 +0100

    Fix rendering issue in BusinessDetails and BusinessCard components

--- client/src/pages/BusinessDetails/index.tsx
+++ client/src/pages/BusinessDetails/index.tsx
@@ -337,7 +337,7 @@ export default function BusinessDetails() {
               >
                 {openingHoursCalendar.map((day) => {
                   return (
-                    <>
+                    <div className="ntw w-full" key={day.day}>
                       <div className="ntw w-full daysOfWeek px-20 ">
                         <FlexRowStartBtw className="w-full">
                           <span
@@ -377,7 +377,7 @@ export default function BusinessDetails() {
                           </span>
                         </FlexRowEnd>
                       </div>
-                    </>
+                    </div>
                   );
                 })}
               </div>
