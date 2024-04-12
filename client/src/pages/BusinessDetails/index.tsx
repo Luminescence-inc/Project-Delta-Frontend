0b998cb99516aea9c43fa4f71c65331f03a90539
benrobo <alumonabenaiah71@gmail.com>
2024-04-12 18:25:04 +0100

    Add Fragment wrapper to improve performance

--- client/src/pages/BusinessDetails/index.tsx
+++ client/src/pages/BusinessDetails/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import {
   FlexColStart,
   FlexRowCenter,
@@ -334,7 +334,7 @@ export default function BusinessDetails() {
               >
                 {openingHoursCalendar.map((day) => {
                   return (
-                    <div className="ntw w-full" key={day.day}>
+                    <Fragment key={day.day}>
                       <div className="ntw w-full daysOfWeek px-20 ">
                         <FlexRowStartBtw className="w-full">
                           <span
@@ -374,7 +374,7 @@ export default function BusinessDetails() {
                           </span>
                         </FlexRowEnd>
                       </div>
-                    </div>
+                    </Fragment>
                   );
                 })}
               </div>
