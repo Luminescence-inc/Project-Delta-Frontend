e004241db56feb7ac45b350b1b8f9acf536c4c63
benrobo <alumonabenaiah71@gmail.com>
2024-04-18 10:25:46 +0100

    Add useAuth hook for authentication

---
+++ client/src/hooks/useAuth.ts
@@ -0,0 +1,36 @@
+import { getUserDetails } from "api/auth";
+import { useDataCtx } from "context/DataCtx";
+import { useEffect, useState } from "react";
+import { TOKEN_NAME, UserDetails } from "types/auth";
+
+export const useAuth = () => {
+  const { isAuth } = useDataCtx();
+  const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (isAuth) {
+      fetchUserInfo();
+    }
+  }, [isAuth]);
+
+  const fetchUserInfo = async () => {
+    try {
+      const authToken = localStorage.getItem(TOKEN_NAME) as string;
+      const response = await getUserDetails(authToken!);
+      const resData = response.data?.data?.userDetails;
+
+      setUserDetails(resData);
+      setLoading(false);
+    } catch (e: any) {
+      const msg = e.response.data.message;
+      console.error(`Error fetching user details: ${msg}`);
+      setLoading(false);
+    }
+  };
+
+  return {
+    loading,
+    userDetails,
+  };
+};
