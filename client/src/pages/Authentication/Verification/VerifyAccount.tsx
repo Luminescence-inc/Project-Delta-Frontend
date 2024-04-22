4eafeea550eda88af18d17fcfae68968d549061e
benrobo <alumonabenaiah71@gmail.com>
2024-04-22 16:48:29 +0100

    Refactor VerifyAccount component styling

--- client/src/pages/Authentication/Verification/VerifyAccount.tsx
+++ client/src/pages/Authentication/Verification/VerifyAccount.tsx
@@ -1,16 +1,16 @@
 /** @format */
-
-import '../ForgotPassword/ForgotPassword.scss';
+import { FlexColCenter } from "components/Flex";
 
 const VerifiedAccount = () => {
-
   return (
-    <div className='login'>
-      <div className='card'>
-      <h4>Your Account is not Verified</h4>
-      <span>Check your Email for Verification Link</span>
+    <FlexColCenter className="w-full h-full bg-gray-102 py-[80px] px-[16px] pb-[150px] ">
+      <div className="w-full rounded-[8px] pt-[24px] px-[16px] pb-[32px]  bg-white-100 text-center ">
+        <h4 className="text-[16px] font-bold leading-[24px] font-hnM mb-[24px]">
+          Your Account is not Verified
+        </h4>
+        <span>Check your Email for Verification Link</span>
       </div>
-    </div>
+    </FlexColCenter>
   );
 };
 
