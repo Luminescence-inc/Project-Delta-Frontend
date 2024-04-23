c6cd676b84b08919f308500efcb35480098a1c56
benrobo <alumonabenaiah71@gmail.com>
2024-04-23 14:42:43 +0100

    Refactor ErrorComponent to use a typed prop and add a ref

--- client/src/pages/Authentication/ErrorComponent.tsx
+++ client/src/pages/Authentication/ErrorComponent.tsx
@@ -1,5 +1,14 @@
-const ErrorComponent = ({ value }: { value: string }) => {
-  return <span className="text-red-305 flex text-[13px]">{value}</span>;
+interface IErrorComponent {
+  value: string;
+  _ref?: React.RefObject<HTMLDivElement>;
+}
+
+const ErrorComponent = ({ value, _ref }: IErrorComponent) => {
+  return (
+    <span ref={_ref} className="text-red-305 flex text-[13px]">
+      {value}
+    </span>
+  );
 };
 
 export default ErrorComponent;
