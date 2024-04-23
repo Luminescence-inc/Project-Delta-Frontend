780e563a9f702b13df1d09666624a481d7c95b38
benrobo <alumonabenaiah71@gmail.com>
2024-04-23 17:51:58 +0100

    Refactor OperationInfo component

--- client/src/pages/Authentication/Signup/RegisterBusiness/OperationInfo.tsx
+++ client/src/pages/Authentication/Signup/RegisterBusiness/OperationInfo.tsx
@@ -1,17 +1,21 @@
 /** @format */
 
-import { BusinessProfileFormikPropsValues } from 'types/business';
-import { FormikProps } from 'formik';
-import { FC } from 'react';
-import { DAYS_OF_OPERATIONS_OPTIONS, OPERATING_TIME_OPTIONS } from 'utils/business-profile-utils';
-import PhoneIcon from 'assets/icons/phone-icon.svg?react';
-import MailIcon from 'assets/icons/mail-icon.svg?react';
-import Input from 'components/Input/Input';
-import Button from 'components/Button/Button';
-import Select from 'components/Input/Select';
-import MultiSelect from 'components/Input/MultiSelect';
-import '../Signup.scss';
-import Spinner from 'components/Spinner/Spinner';
+import { BusinessProfileFormikPropsValues } from "types/business";
+import { FormikProps } from "formik";
+import { FC } from "react";
+import {
+  DAYS_OF_OPERATIONS_OPTIONS,
+  OPERATING_TIME_OPTIONS,
+} from "utils/business-profile-utils";
+import PhoneIcon from "assets/icons/phone-icon.svg?react";
+import MailIcon from "assets/icons/mail-icon.svg?react";
+import Input from "components/Input/Input";
+import Button from "components/ui/button";
+import Select from "components/Input/Select";
+import MultiSelect from "components/Input/MultiSelect";
+import "../Signup.scss";
+import Spinner from "components/Spinner/Spinner";
+import { FlexColStart, FlexColStartCenter } from "components/Flex";
 
 interface OperationInfoProps {
   formik: FormikProps<BusinessProfileFormikPropsValues>;
@@ -19,71 +23,85 @@ interface OperationInfoProps {
   isLoading: boolean;
 }
 
+const OperationInfo: FC<OperationInfoProps> = ({
+  formik,
+  businessId,
+  isLoading,
+}) => {
+  const filterDaysOfOperation = DAYS_OF_OPERATIONS_OPTIONS.filter((days) => {
+    const d = formik.values.daysOfOperation as Array<string>;
+    return d.includes(days.value);
+  });
 
-const OperationInfo: FC<OperationInfoProps> = ({formik, businessId, isLoading}) => {
-
-  const filterDaysOfOperation = DAYS_OF_OPERATIONS_OPTIONS.filter((days)=>{
-    const d = formik.values.daysOfOperation as Array<string>
-    return d.includes(days.value)
-  })
-  
   return (
-    <div className='signup'>
-      <div className='card'>
-        <h4>Operation Info</h4>
+    <FlexColStart className="w-full h-full bg-gray-200 pt-[40px] px-[16px] pb-[150px] ">
+      <FlexColStartCenter className="w-full h-auto text-center bg-white-100 rounded-[8px] pt-[24px] px-[16px] pb-[23px] ">
+        <h4 className="text-[16px] text-center font-normal font-hnM leading-[24px] mb-[24px] ">
+          Operation Info
+        </h4>
 
         <Input
-          type='tel'
-          name='phoneNumber'
+          type="tel"
+          name="phoneNumber"
           value={formik.values.phoneNumber}
           onChange={formik.handleChange}
-          label='Business Phone Number'
-          icon={<PhoneIcon className='input-icon' />}
-          placeholder='Enter Phone Number'
+          label="Business Phone Number"
+          icon={<PhoneIcon className="input-icon" />}
+          placeholder="Enter Phone Number"
         />
 
         <Input
-          type='email'
-          name='businessEmail'
+          type="email"
+          name="businessEmail"
           value={formik.values.businessEmail}
           onChange={formik.handleChange}
-          label='Business Email'
-          icon={<MailIcon className='input-icon' />}
-          placeholder='Enter Business Email'
+          label="Business Email"
+          icon={<MailIcon className="input-icon" />}
+          placeholder="Enter Business Email"
         />
 
         <Select
-          label='Business open time'
-          name='openTime'
+          label="Business open time"
+          name="openTime"
           formikValue={formik.values.openTime}
           formik={formik}
-          placeholder={'Business open time'}
+          placeholder={"Business open time"}
           options={OPERATING_TIME_OPTIONS}
         />
 
         <Select
-          label='Business close time'
-          name='closeTime'
+          label="Business close time"
+          name="closeTime"
           formikValue={formik.values.closeTime}
           formik={formik}
-          placeholder={'Business close time'}
+          placeholder={"Business close time"}
           options={OPERATING_TIME_OPTIONS}
         />
 
         <MultiSelect
-          label='Days of operation'
-          placeholder={'Days of operation'}
-          name='daysOfOperation'
+          label="Days of operation"
+          placeholder={"Days of operation"}
+          name="daysOfOperation"
           formikValue={filterDaysOfOperation}
           formik={formik}
           options={DAYS_OF_OPERATIONS_OPTIONS}
         />
 
-        {!isLoading && <Button type='submit' onClick={formik.submitForm} label={businessId?`Update Profile`:`Submit Profile`} variant='primary' size='lg' />}
-        {isLoading && <Button type='submit'  label={Spinner()} variant='primary' size='lg' disabled={true} />}
-
-      </div>
-    </div>
+        <Button
+          intent={"primary"}
+          size={"lg"}
+          className="w-full rounded-[5px] mt-5"
+          isLoading={isLoading}
+          spinnerColor="#000"
+          onClick={formik.submitForm as any}
+          type="submit"
+        >
+          <span className="text-[14px] font-hnM">
+            {businessId ? `Update Profile` : `Submit Profile`}
+          </span>
+        </Button>
+      </FlexColStartCenter>
+    </FlexColStart>
   );
 };
 
