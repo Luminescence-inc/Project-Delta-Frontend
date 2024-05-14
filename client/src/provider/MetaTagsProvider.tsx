3a355b0cfbdf550c43ae923f751b67b6339b6f4e
benrobo <alumonabenaiah71@gmail.com>
2024-05-14 14:35:23 +0100

    created a metatagprovider reusable component

---
+++ client/src/provider/MetaTagsProvider.tsx
@@ -0,0 +1,87 @@
+import SITE_CONFIG from "@/config/site";
+import { Helmet } from "react-helmet";
+
+interface IMetaTagsProviderProps {
+  title: string;
+  description?: string;
+  image?: string;
+  url?: string;
+  keywords?: string;
+  author?: string;
+  canonical?: string;
+  robots?: "index, follow" | "noindex, nofollow";
+  og?: {
+    title?: string;
+    description?: string;
+    image?: string;
+    url?: string;
+    type?: string;
+    site_name?: string;
+  };
+}
+
+const MetaTagsProvider = (props: IMetaTagsProviderProps) => {
+  return (
+    <>
+      <Helmet>
+        <title>{props.title ?? SITE_CONFIG.title}</title>
+        <meta
+          name="description"
+          content={props.description ?? SITE_CONFIG.description}
+        />
+        <meta
+          name="keywords"
+          content={props.keywords ?? SITE_CONFIG.keywords}
+        />
+        <meta name="author" content={props.author ?? SITE_CONFIG.name} />
+        <meta name="robots" content={props.robots} />
+
+        <meta
+          property="og:title"
+          content={props.og?.title ?? SITE_CONFIG.title}
+        />
+        <meta
+          property="og:description"
+          content={props.og?.description ?? SITE_CONFIG.description}
+        />
+        <meta
+          property="og:image"
+          content={props.og?.image ?? SITE_CONFIG.image}
+        />
+        <meta property="og:url" content={props.og?.url} />
+        <meta property="og:type" content={props.og?.type} />
+        <meta
+          property="og:site_name"
+          content={props.og?.site_name ?? SITE_CONFIG.name}
+        />
+
+        {/* twitter cards */}
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="twitter:site"
+          content={props.og?.site_name ?? "@bizconnect24"}
+        />
+        <meta
+          name="twitter:title"
+          content={props.og?.title ?? "bizconnect24"}
+        />
+        <meta
+          name="twitter:description"
+          content={props.og?.description ?? SITE_CONFIG.description}
+        />
+        <meta
+          name="twitter:image"
+          content={props.og?.image ?? SITE_CONFIG.image}
+        />
+
+        {/* canonical url */}
+        {props?.canonical && <link rel="canonical" href={props.canonical} />}
+
+        {/* favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Helmet>
+    </>
+  );
+};
+
+export default MetaTagsProvider;
