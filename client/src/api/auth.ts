c0b2822d22f968be15e444fddaef2cb753ddb888
benrobo <alumonabenaiah71@gmail.com>
2024-05-27 17:11:45 +0100

    refactor: Update auth API endpoints to use relative paths

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -4,17 +4,16 @@ import {
   SignUpData,
   updateUserData,
 } from "@/types/auth";
-import { bizConnectAPI } from "@/config";
-import axios from "axios";
+import $axios from "./axios";
 
 export const registerUser = (data: SignUpData) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/register`;
-  return axios.post(url, data);
+  const url = `/user/register`;
+  return $axios.post(url, data);
 };
 
 export const loginUser = (data: LogInData) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/login`;
-  return axios.post(url, data);
+  const url = `/user/login`;
+  return $axios.post(url, data);
 };
 
 export const resetUserPassword = (
@@ -22,52 +21,36 @@ export const resetUserPassword = (
   uniqueString: string,
   data: ResetPasswordData
 ) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/reset_password/${userId}/${uniqueString}`;
-  return axios.post(url, { newPassword: data.password });
+  const url = `/user/reset_password/${userId}/${uniqueString}`;
+  return $axios.post(url, { newPassword: data.password });
 };
 
 export const verifyUserAccount = (userId: string, uniqueString: string) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/verify/${userId}/${uniqueString}`;
-  return axios.get(url);
+  const url = `/user/verify/${userId}/${uniqueString}`;
+  return $axios.get(url);
 };
 
-export const isAuthenticated = (token: string, userId: string) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/authenticated/${userId}`;
-  return axios.get(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const isAuthenticated = (userId: string) => {
+  const url = `/user/authenticated/${userId}`;
+  return $axios.get(url);
 };
 
-export const logOut = (token: string, userId: string) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/logout/${userId}`;
-  return axios.get(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const logOut = (userId: string) => {
+  const url = `/user/logout/${userId}`;
+  return $axios.get(url);
 };
 
 export const generateVerificationEmail = (email: string) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/generate/forgot_password/${email}`;
-  return axios.get(url);
+  const url = `/user/generate/forgot_password/${email}`;
+  return $axios.get(url);
 };
 
-export const getUserDetails = (token: string) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/details`;
-  return axios.get(url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const getUserDetails = () => {
+  const url = `/user/details`;
+  return $axios.get(url);
 };
 
-export const updateUserDetails = (token: string, data: updateUserData) => {
-  const url = `${bizConnectAPI.baseURL}/api/user/details/update`;
-  return axios.post(url, data, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const updateUserDetails = (data: updateUserData) => {
+  const url = `/user/details/update`;
+  return $axios.post(url, data);
 };
