020c749e7879867408e106f848492ed392b24202
benrobo <alumonabenaiah71@gmail.com>
2024-05-28 21:33:56 +0100

    refactor: Update API endpoints to use kebab-case for consistency

--- client/src/api/business.ts
+++ client/src/api/business.ts
@@ -6,17 +6,17 @@ import { bizConnectAPI } from "@/config";
 import $axios from "./axios";
 
 export const allBusinessCategories = () => {
-  const url = `${bizConnectAPI.baseURL}/api/business_profile/categories`;
+  const url = `${bizConnectAPI.baseURL}/api/business-profile/categories`;
   return $axios.get(url);
 };
 
 export const getAllBusinessCategories = () => {
-  const url = `/business_profile/categories`;
+  const url = `/business-profile/categories`;
   return $axios.get(url);
 };
 
 export const getUploadSignature = (folder: string) => {
-  const url = `/business_profile/upload_signature`;
+  const url = `/business-profile/upload-signature`;
   return $axios.get(url, {
     params: {
       folderName: folder,
@@ -25,12 +25,12 @@ export const getUploadSignature = (folder: string) => {
 };
 
 export const createBusinessProfile = (data: BusinessCreationBody) => {
-  const url = `/business_profile/create`;
+  const url = `/business-profile/create`;
   return $axios.post(url, data);
 };
 
 export const getUserBusinessProfileList = () => {
-  const url = `/business_profile/list`;
+  const url = `/business-profile/list`;
   return $axios.get(url);
 };
 
@@ -40,17 +40,17 @@ export const searchForBusinesses = (queryParams: string) => {
 };
 
 export const getBusinessProfileById = (id: string) => {
-  const url = `/business_profile/list/${id}`;
+  const url = `/business-profile/list/${id}`;
   return $axios.get(url);
 };
 
 export const getBusinsessCategories = () => {
-  const url = `/business_profile/categories`;
+  const url = `/business-profile/categories`;
   return $axios.get(url);
 };
 
 export const getUserBusinessProfileDetail = (id: string) => {
-  const url = `/business_profile/list/${id}`;
+  const url = `/business-profile/list/${id}`;
   return $axios.get(url);
 };
 
@@ -58,16 +58,16 @@ export const updateUserBusinessProfileDetail = (
   id: string,
   data: BusinessCreationBody
 ) => {
-  const url = `/business_profile/update/${id}`;
+  const url = `/business-profile/update/${id}`;
   return $axios.post(url, data);
 };
 
 export const deleteUserBusinessProfile = (profileId: string) => {
-  const url = `/business_profile/delete/${profileId}`;
+  const url = `/business-profile/delete/${profileId}`;
   return $axios.delete(url);
 };
 
 export const submitContactRequest = (data: ContactSupportDataSchema) => {
-  const url = `/business_profile/contact/`;
+  const url = `/business-profile/contact/`;
   return $axios.post(url, data);
 };
