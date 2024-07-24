457e0e5284346ffa17d5f4d21ceb9ed9d7470dd2
geraldkachi <fitzgeraldkachi@gmail.com>
2024-07-24 13:44:54 +0100

    gerald test branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,9 @@ import {
   HeaderBusinessBtn,
   HeaderOnboardingComp,
   HeaderSearchComp,
-} from "@/modules/home/components/HeaderComp";
+} from "@/modules/home/components/HeaderComp"; 
+
+// gerald first branch and pr
 
 const Home = () => {
   return (
