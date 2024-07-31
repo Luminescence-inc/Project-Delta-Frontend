111f2d1eb1534c32d46235d34baeabbf93614d45
geraldkachi <fitzgeraldkachi@gmail.com>
2024-07-31 17:01:56 +0100

    fresh update

--- src/components/BusinessFilter.tsx
+++ src/components/BusinessFilter.tsx
@@ -267,7 +267,7 @@ const BusinessesFilterComponent = ({
     >
       <div className="w-full h-auto flex flex-col items-start justify-start px-[20px] py-[50px] bg-white-100">
         <div className="w-full h-auto flex flex-row items-center justify-between ">
-          <h2 className="text-[30px] font-bold font-inter">Filter</h2>
+          <h2 className="text-[30px] font-bold font-inter text-blue-200">Filter</h2>
           <button
             className="be-none border-none outline-none cursor-pointer"
             onClick={() => {
